24db64643222592bf9fb182d59a5ae3bd15dbadc
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-10-03 17:00:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GetSSID
+ 
---
+++ app/src/main/java/com/gzeinnumer/getssid/MainActivity.java
@@ -0,0 +1,54 @@
+package com.gzeinnumer.getssid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    private static final int LOCATION = 1;
+    protected void onStart() {
+        super.onStart();
+        //Assume you want to read the SSID when the activity is started
+        tryToReadSSID();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED && requestCode == LOCATION){
+            //User allowed the location and you can read it now
+            tryToReadSSID();
+        }
+    }
+
+    private void tryToReadSSID() {
+        //If requested permission isn't Granted yet
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Request permission from user
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION);
+        }else{//Permission already granted
+            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if(wifiInfo.getSupplicantState() == SupplicantState.COMPLETED){
+                String ssid = wifiInfo.getSSID();//Here you can access your SSID
+                System.out.println(ssid);
+                Log.d(TAG, "onCreate: "+ssid);
+            }
+        }
+    }
+}
